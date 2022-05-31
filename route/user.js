4bdbc3f7730e49578f8f53816b1d190e78311cc1
Willian Watanabe <watinha@gmail.com>
2022-05-31 20:27:37 -0300

    using express-validator

--- route/user.js
+++ route/user.js
@@ -1,15 +1,27 @@
 import express from 'express';
+import { query, body, validationResult } from 'express-validator';
+
 import user from '../model/user.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', query('name').isLength({ min: 3 }), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: '...' });
+  }
+
   const { name, age } = req.query,
         users = await user.find({ name, age });
   res.json(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', body('name').isLength({ min: 3 }), body('age').isInt().toInt(), async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ message: '...' });
+  }
+
   const { name, age } = req.body,
         new_user = new user({ name, age });
 
